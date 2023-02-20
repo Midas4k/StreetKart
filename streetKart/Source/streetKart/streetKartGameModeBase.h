752050b43770c2fc3ef7f3284dbdbb9cb6c45b6e
Midas4k <Ollie201436@hotmail.co.uk>
2023-02-20 15:58:48 +0000

    initial commit

---
+++ streetKart/Source/streetKart/streetKartGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "streetKartGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STREETKART_API AstreetKartGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
